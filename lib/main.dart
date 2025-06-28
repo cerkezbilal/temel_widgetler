ad2ab672a8c7d2dfcba532dca7d895b47e669c91
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2025-06-28 23:52:50 +0300

    calismalar eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,33 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Resim çalışması"),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              child: FadeInImage.assetNetwork(
+                placeholder: "resimler/loading.gif",
+                image:
+                    "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              height: 300,
+              width: 300,
+              child: Placeholder(color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
